c26bd5171b55f04d80ca1f81a66471e1eb0a878d
Daniel Mihai <danielmihai4@yahoo.com>
2015-09-05 16:26:22 +0200

    default table view for wish lists

---
+++ Gimme/ParseStarterProject/WishlistTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WishlistTableViewCell.swift
+//  Gimme
+//
+//  Created by Daniel Mihai on 9/5/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class WishlistTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
