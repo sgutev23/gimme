308509abfa1112eca5e3e2cec81ab02df50e04f3
sgutev23 <sgutev23@gmail.com>
2015-09-05 16:39:14 +0200

    test

--- Gimme/ParseStarterProject/WishlistTableViewController.swift
+++ Gimme/ParseStarterProject/WishlistTableViewController.swift
@@ -13,6 +13,7 @@ class WishlistTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print ("hello")
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
