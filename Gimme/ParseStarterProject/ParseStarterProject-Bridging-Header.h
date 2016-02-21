845d8ab77d19adb01e62b364b026b6ef6e93b938
sgutev23 <sgutev23@gmail.com>
2016-02-21 10:52:45 +0100

    removing parse

--- Gimme/ParseStarterProject/ParseStarterProject-Bridging-Header.h
+++ Gimme/ParseStarterProject/ParseStarterProject-Bridging-Header.h
@@ -8,7 +8,6 @@
 #define ParseStarterProject_Bridging_Header_h
 
 // If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
-#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 
